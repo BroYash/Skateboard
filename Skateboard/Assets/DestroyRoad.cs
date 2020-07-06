75549735dfdd971bd337ad83f18115685ee39234
BroYash <yashjeetbajwa8@gmail.com>
2020-07-06 15:31:58 +1200

    Destroy road after player passes it

---
+++ Skateboard/Assets/DestroyRoad.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyRoad : MonoBehaviour
+{
+    public GameObject _object;
+    private playerController pc;
+
+    public void Start()
+    {
+        pc = FindObjectOfType<playerController>();
+    }
+
+
+
+    private void Update()
+    {
+        if(pc.transform.position.z > _object.transform.position.z)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
--- Skateboard/Assets/playerController.cs
+++ Skateboard/Assets/playerController.cs
@@ -62,6 +62,4 @@ public class playerController : MonoBehaviour
         sk.isGrounded = false;
     }
 
-
-
 }
