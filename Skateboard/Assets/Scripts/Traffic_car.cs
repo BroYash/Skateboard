2e743a2e53146c236cad9aef7304ccceb45c98df
BroYash <yashjeetbajwa8@gmail.com>
2020-07-07 15:01:32 +1200

    Cars added, spawning points

---
+++ Skateboard/Assets/Scripts/Traffic_car.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Traffic_car : MonoBehaviour
+{
+    public float speed = 15f;
+
+    private Rigidbody rb;
+    private Collider coll;
+    private playerController pc;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        coll = GetComponent<Collider>();
+        pc = FindObjectOfType<playerController>();
+    }
+
+    private void Update()
+    {
+
+        rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, speed);
+        if(pc.transform.position.z > this.transform.position.z)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (pc != null)
+        {
+            pc.ToggleDead();
+        }
+
+    }
+}
---
+++ Skateboard/Assets/SpawnCars.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCars : MonoBehaviour
+{
+
+    public List<GameObject> spawnLeft;
+    public List<GameObject> spawnRight;
+    public GameObject prefab;
+
+    private void Awake()
+    {
+        SpawnLeft();
+        SpawnRight();
+
+    }
+
+    private void SpawnLeft()
+    {
+        int i = Random.Range(0, spawnLeft.Count);
+        GameObject car = Instantiate(prefab, spawnLeft[i].transform.position, Quaternion.Euler(0f, 180f, 0f));
+        car.GetComponent<Traffic_car>().speed = -car.GetComponent<Traffic_car>().speed;
+    }
+    private void SpawnRight()
+    {
+        int j = Random.Range(0, spawnRight.Count);
+        Instantiate(prefab, spawnRight[j].transform.position, Quaternion.identity);
+    }
+
+
+}
