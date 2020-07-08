558aef561e51345c5a4487cdb3020103ca9d6432
yuvrajbajwa10 <yuvrajbajwa8@gmail.com>
2020-07-08 16:09:35 +1200

    added more variability to peds spawner.cs

--- Skateboard/Assets/Scripts/LevelDesign/pedSpawner.cs
+++ Skateboard/Assets/Scripts/LevelDesign/pedSpawner.cs
@@ -10,6 +10,8 @@ public class pedSpawner : MonoBehaviour
     public int column = 2;
     public int size = 6;
     public float distance = 5.0f;
+    public int spreadx;
+    public int spreadz;
 
     public GameObject charactersPrefab;
     
@@ -94,12 +96,13 @@ public class pedSpawner : MonoBehaviour
 
         int c = 0;
         int ranx;
-
+        int ranz;
 
         for (int i = 0; i < size; i++)
         {
-            ranx = random.Next(-1, 1);
-            pedList[i].GetComponent<Transform>().position = new Vector3(nx - ranx, ny, nz - ranx);
+            ranx = random.Next(-spreadx, spreadx);
+            ranz = random.Next(-spreadz, spreadz);
+            pedList[i].GetComponent<Transform>().position = new Vector3(nx + ranx, ny, nz + ranz);
 
             nx += distance;
             c++;
