5111c4eeb2d2cfcad3e7884e9fe71286e4e59882
BroYash <yashjeetbajwa8@gmail.com>
2020-07-15 14:11:46 +1200

    Revert "ss"
    This reverts commit cb1fa23c489dde74a1c4f752bf1419d8be805bbf.

--- Skateboard/Assets/GameObjectPool.cs
+++ Skateboard/Assets/GameObjectPool.cs
@@ -133,12 +133,13 @@ public class GameObjectPool : MonoBehaviour
         {
             vehicleIndex = 0;
         }
-        vehicleArray[vehicleIndex].transform.position = vehiclePosition1;  
-        vehiclePosition1 = vehiclePosition1 + new Vector3(0, 0, 97);
+        vehicleArray[vehicleIndex].transform.position = vehiclePosition1;
         vehicleIndex++;
         vehicleArray[vehicleIndex].transform.position = vehiclePosition2;
-        vehiclePosition2 = vehiclePosition2 + new Vector3(0, 0, 97);
         vehicleIndex++;
+        vehiclePosition1 = vehiclePosition1 + new Vector3(0, 0, 97);
+        vehiclePosition2 = vehiclePosition2 + new Vector3(0, 0, 97);
+
     }
 }
 
